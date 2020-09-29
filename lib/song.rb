8306250202fe4616493763feb118599d280b19e5
April Adams <illusionlace@gmail.com>
2020-09-29 13:30:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,44 +7,44 @@ class Song
 def save
     self.class.all << self
   end
-def self.create 
+def self.create
   song = Song.new
   song.save
-  song 
-end 
+  song
+end
 def self.new_by_name(name)
   song = self.new
-  song.name = name 
-  song 
-end 
+  song.name = name
+  song
+end
 def self.create_by_name(name)
   song = self.create
-  song.name = name 
-  song 
-end 
+  song.name = name
+  song
+end
 def self.find_by_name(name)
   self.all.find {|obj|obj.name == name}
-end 
+end
 def self.find_or_create_by_name(name)
   self.find_by_name(name) ||
   self.create_by_name(name)
-end 
+end
 def self.alphabetical
   self.all.sort_by {|song| song.name}
-end 
+end
 def self.new_from_filename(filename)
   songFile = filename.split(/[-.]+/)
   song_new= self.new_by_name(songFile[1].strip)
   song_new.artist_name = songFile[0].strip
   song_new
-end 
+end
 def self.create_from_filename(filename)
   songFile = filename.split(/[-.]+/)
   song_new = self.create_by_name(songFile[1].strip)
-  song_new.artist_name = songFile[0].strip 
+  song_new.artist_name = songFile[0].strip
   song_new
-end 
+end
 def self.destroy_all
   @@all = []
-end 
+end
 end 
